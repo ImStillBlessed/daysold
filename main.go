18a85abfc550ec8221e2b2203bd6365ffda8109e
Oigbochie Blessed <eprince1800@gmail.com>
2025-07-11 21:43:17 +0100

    Implement date validation and flag command-line input for birthday

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 )
@@ -49,6 +50,10 @@ func daysInMonth(year int, month time.Month) int {
 
 func validateDate(dob Birthday) (time.Time, error) {
 	date := time.Date(dob.Year, time.Month(dob.Month), dob.Day, 0, 0, 0, 0, time.UTC)
+	if date.After(time.Now()) {
+		return time.Time{}, fmt.Errorf("date cannot be in the future")
+
+	}
 	if date.Day() != dob.Day || int(date.Month()) != dob.Month || date.Year() != dob.Year {
 		return time.Time{}, fmt.Errorf("invalid Date")
 	}
@@ -71,15 +76,39 @@ func askInput(dob *Birthday) {
 }
 
 func main() {
+	dobFlag := flag.String("dob", "", "Your date of birth in DD-MM-YYYY format")
+	flag.Parse()
+
 	var dob Birthday
 	var age ageBreakdown
-	askInput(&dob)
+	fallback := false
+
+	if *dobFlag != "" {
+		_, err := fmt.Sscanf(*dobFlag, "%d-%d-%d", &dob.Day, &dob.Month, &dob.Year)
+		if err != nil {
+			fmt.Println("Invalid date from flag:", err)
+			fallback = true
+		} else {
+			dob.Date, err = validateDate(dob)
+			if err != nil {
+				fmt.Println("Invalid date from flag:", err)
+				fallback = true
+			}
+		}
+	} else {
+		fallback = true
+	}
+
+	if fallback {
+		askInput(&dob)
+	}
+
 	fmt.Printf("Born on the %d of %s, %d\n", dob.Day, dob.Date.Month(), dob.Year)
 	calculateAge(dob, &age)
-
 	fmt.Printf("\nYou have lived for:\n")
 	fmt.Printf("🟢 %d years and %d months\n", age.Years, age.ExtraMonths)
 	fmt.Printf("📆 %d total months\n", age.TotalMonths)
 	fmt.Printf("📅 %d weeks\n", age.Weeks)
 	fmt.Printf("🕒 %d days\n", age.Days)
+	fmt.Println()
 }
